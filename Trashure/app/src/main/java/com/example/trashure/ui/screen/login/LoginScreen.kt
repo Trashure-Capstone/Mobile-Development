e6d0a3f27db5a38e63dd52dca774c6040f330564
unknown <owen>
2023-05-28 08:57:44 +0700

    add - theme & splash screen done

--- Trashure/app/src/main/java/com/example/trashure/ui/screen/login/LoginScreen.kt
+++ Trashure/app/src/main/java/com/example/trashure/ui/screen/login/LoginScreen.kt
@@ -1,22 +1,66 @@
 package com.example.trashure.ui.screen.login
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.material.Text
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextLayoutInput
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.trashure.R
+import com.example.trashure.ui.theme.Lato
+import com.example.trashure.ui.theme.PrimaryBackgroundColor
+import com.example.trashure.ui.theme.PrimaryColor
 
 @Composable
 @Preview
 fun LoginScreen(
     modifier: Modifier = Modifier
 ) {
-    Box(
-        modifier = modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center,
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(PrimaryBackgroundColor, PrimaryColor)
+                )
+            ),
     ) {
-        Text("login")
+        Image(
+            painter = painterResource(id = R.drawable.image_login),
+            contentDescription = stringResource(id = R.string.image),
+            contentScale = ContentScale.FillWidth,
+            modifier = Modifier.fillMaxWidth()
+        )
+        
+        Text(
+            text = stringResource(id = R.string.sign_in),
+            fontFamily = Lato,
+            fontWeight = FontWeight.Bold,
+            fontSize = 32.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(
+            text = stringResource(id = R.string.sign_in_desc),
+            fontFamily = Lato,
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(30.dp))
+        
     }
 }
