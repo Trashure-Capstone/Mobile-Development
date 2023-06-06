6157dcbdc3566feb751f87787056c4d39e29a428
hilalhmdy <mhdhilalhmdy15@gmail.com>
2023-06-07 00:35:42 +0700

    add profile screen

--- Trashure/app/src/main/java/com/example/trashure/ui/screen/profile/ChangePasswordScreen.kt
+++ Trashure/app/src/main/java/com/example/trashure/ui/screen/profile/ChangePasswordScreen.kt
@@ -1,11 +1,96 @@
 package com.example.trashure.ui.screen.profile
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.trashure.R
+import com.example.trashure.ui.components.MyTopBar
+import com.example.trashure.ui.theme.TrashureTheme
 
 @Composable
 fun ChangePasswordScreen(
+    navController: NavHostController = rememberNavController(),
     modifier: Modifier = Modifier
-) {
+){
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+    Scaffold (
+        topBar = {
+            MyTopBar(title = stringResource(R.string.change_password))
+        },
+        modifier = modifier
+    ){innerPadding ->
+        Column(
+            verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .padding(innerPadding)
+                .fillMaxSize()
+        ) {
+            Spacer(modifier = Modifier.height(30.dp))
+            Text(
+                text = "Password Baru",
+                maxLines = 1,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 56.dp)
+            )
+            OutLineTextFieldSample()
+            Text(
+                text = "Confirm New Password",
+                maxLines = 1,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 56.dp)
+            )
+            OutLineTextFieldSample()
+            Spacer(modifier = Modifier.height(30.dp))
+            ButtonSave()
+        }
 
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ChangePasswordScreenPreview(){
+    TrashureTheme {
+        ChangePasswordScreen()
+    }
 }
